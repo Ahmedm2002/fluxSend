e510b657f1f1e89eb9731f75f6c3156c0f324e93
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-09-21 03:11:35 +0500

    added basic folder structure

---
+++ src/screens/onBoarding/intro2.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+function intro2() {}
+
+export default intro2;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ src/screens/splash.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+function splash() {}
+
+export default splash;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
